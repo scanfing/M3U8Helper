ccd480b4dc2f5288bcda33b5b03cd56ffd7b9ea6
Mxs <scanfing@foxmail.com>
2023-09-24 22:34:44 +0800

    临时保存

---
+++ Downloader/ViewModels/ExploreViewModel.cs
@@ -0,0 +1,56 @@
+﻿using M3U8Downloader.Infrastruction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace M3U8Downloader.ViewModels
+{
+    public class ExploreViewModel : NotifyObject
+    {
+        #region Constructors
+
+        public ExploreViewModel()
+        {
+            NavigateToCommand = new WpfCommand<string>(OnRequestOpenUrl, (e) => true);
+        }
+
+        #endregion Constructors
+
+        #region Properties
+
+        public WebBrowser Browser { get; set; } = new WebBrowser();
+
+        public ICommand NavigateToCommand { get; private set; }
+
+        #endregion Properties
+
+        #region Methods
+
+        public void SetupBrowser()
+        {
+            Browser.Navigated += Browser_Navigated;
+            Browser.LoadCompleted += Browser_LoadCompleted;
+        }
+
+        private void Browser_LoadCompleted(object sender, System.Windows.Navigation.NavigationEventArgs e)
+        {
+            System.Diagnostics.Trace.TraceInformation($"LoadCompleted {e.Uri}");
+        }
+
+        private void Browser_Navigated(object sender, System.Windows.Navigation.NavigationEventArgs e)
+        {
+            System.Diagnostics.Trace.TraceInformation($"Navigated {e.Uri}");
+        }
+
+        private void OnRequestOpenUrl(string url)
+        {
+            Browser.Navigate(url);
+        }
+
+        #endregion Methods
+    }
+}
---
+++ Downloader/Views/ExploreView.xaml.cs
@@ -0,0 +1,51 @@
+﻿using M3U8Downloader.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace M3U8Downloader.Views
+{
+    /// <summary>
+    /// ExploreView.xaml 的交互逻辑
+    /// </summary>
+    public partial class ExploreView : Window
+    {
+        #region Fields
+
+        private ExploreViewModel _viewModel;
+
+        #endregion Fields
+
+        #region Constructors
+
+        public ExploreView()
+        {
+            InitializeComponent();
+            _viewModel = DataContext as ExploreViewModel;
+            _viewModel.Browser = webBorwser;
+
+            Loaded += ExploreView_Loaded;
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        private void ExploreView_Loaded(object sender, RoutedEventArgs e)
+        {
+            _viewModel?.SetupBrowser();
+        }
+
+        #endregion Methods
+    }
+}
